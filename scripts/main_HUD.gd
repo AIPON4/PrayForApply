08a4987776f3b0198a15508dae7c6e8f5df4ba1b
AIPON4 <72207793+AIPON4@users.noreply.github.com>
2022-05-01 10:58:40 +0900

    いろいろ
    フォント，シーン，スクリプト，テクスチャ盛り合わせ．
    わーい．

--- scripts/main_HUD.gd
+++ scripts/main_HUD.gd
@@ -1,10 +1,34 @@
 extends CanvasLayer
 
-signal generate_job_seeker
+signal generate_applyer_status
+signal generate_applyer_graphics
+signal posting_start
+
+# 求職者の面接結果
+signal apply    # 状態0
+signal stay     # 状態1
+signal pray     # 状態2
 
 func _ready():
     pass
 
-func _on_GenerateButton_pressed():
-    emit_signal("generate_job_seeker")
+func _on_apply_pressed():
+    emit_signal("generate_applyer_status")
+    emit_signal("generate_applyer_graphics")
+    emit_signal("posting_start")
+    emit_signal("apply", 0)
+    pass
+
+func _on_stay_pressed():
+    emit_signal("generate_applyer_status")
+    emit_signal("generate_applyer_graphics")
+    emit_signal("posting_start")
+    emit_signal("stay", 1)
+    pass
+
+func _on_pray_pressed():
+    emit_signal("generate_applyer_status")
+    emit_signal("generate_applyer_graphics")
+    emit_signal("posting_start")
+    emit_signal("pray", 2)
     pass
